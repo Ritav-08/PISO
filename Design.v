f0c1e04205e2093f22b0499780702aa922b3711c
Ritav <ritav08092004@gmail.com>
2026-05-09 10:43:02 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,27 +1,25 @@
 module PISO(
-   input [31:0]din_i, 
-   input load_i, 
-   input clk_i, 
-   input rst_i, 
-   output reg dout_o
+   input [31:0] din_i, 
+   input        load_i, 
+   input        clk_i, 
+   input        rst_i, 
+   output reg   dout_o
 );
 
 //net(s)
-reg [31:0]DATA;
+reg [31:0] DATA;
 
 //PISO Register
 always@(posedge clk_i) begin
    if(rst_i)
       dout_o <= 1'b0;
    else if(load_i) begin
-      DATA <= din_i;
-      //dout_o <= 1'b0;
+      DATA   <= din_i;
    end
    else begin
       dout_o <= DATA[0];
-      DATA <= {1'b0, DATA[31:1]};
+      DATA   <= {1'b0, DATA[31:1]};
    end
 end
-
 
 endmodule
