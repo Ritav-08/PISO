a57ffd5e9b6d61c4e67b5e4234229e86841e0487
Ritav <ritav08092004@gmail.com>
2026-05-09 10:45:00 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,37 +1,38 @@
 module tb_PISO();
-reg load_ti;
-reg clk_ti;
-reg rst_ti;
-reg [31:0]din_ti;
-wire dout_to;
+reg        load_ti;
+reg        clk_ti;
+reg        rst_ti;
+reg [31:0] din_ti;
+wire       dout_to;
 
 //instantiation
-PISO DUT(.din_i(din_ti), 
+PISO DUT(
+   .din_i(din_ti), 
    .load_i(load_ti), 
    .clk_i(clk_ti), 
    .rst_i(rst_ti), 
-   .dout_o(dout_to));
+   .dout_o(dout_to)
+);
 
 //clock
 initial begin
-   clk_ti = 1'b0;
-   forever
-      #5 clk_ti = ~clk_ti;
+   clk_ti            = 1'b0;
+   forever #5 clk_ti = ~clk_ti;
 end
 
 //feeding
 initial begin
-rst_ti = 1'b1;
+rst_ti  = 1'b1;
 load_ti = 1'b0;
-din_ti = 32'h00000000;
-#10 rst_ti = 1'b0;
-load_ti = 1'b1;
-din_ti = 32'h84fac960;
-#10 load_ti = 1'b0;
-din_ti = 32'hffffffff;
+din_ti  = 32'h00000000;
+#10 rst_ti   = 1'b0;
+load_ti      = 1'b1;
+din_ti       = 32'h84fac960;
+#10 load_ti  = 1'b0;
+din_ti       = 32'hffffffff;
 #330 load_ti = 1'b1;
-#10 load_ti = 1'b0;
-#50 load_ti = 1'b1;
+#10 load_ti  = 1'b0;
+#50 load_ti  = 1'b1;
 #10 $finish;
 end
 
